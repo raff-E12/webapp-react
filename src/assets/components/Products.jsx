1f929455ebd8481751797731f27483d810f01268
Raff_12 <raffelecoppola381@gmail.com>
2025-05-12 17:18:19 +0200

    feat: Modifica di layout..

--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import placeholder from "../../../public/imgs/img_placeholder.png"
 
 export default function Products() {
   return (
@@ -8,19 +9,19 @@ export default function Products() {
       <h2>Film in Evidenza</h2>
       <div class="product-grid">
         <div class="product">
-          <img src="https://via.placeholder.com/250x200" alt="Libro 1" />
+          <img src={placeholder} alt="Libro 1" />
           <h3>Titolo Libro 1</h3>
           <p>Autore: Mario Rossi</p>
           <button class="btn">Acquista Ora</button>
         </div>
         <div class="product">
-          <img src="https://via.placeholder.com/250x200" alt="Libro 2" />
+          <img src={placeholder} alt="Libro 2" />
           <h3>Titolo Libro 2</h3>
           <p>Autore: Laura Bianchi</p>
           <button class="btn">Acquista Ora</button>
         </div>
         <div class="product">
-          <img src="https://via.placeholder.com/250x200" alt="Libro 3" />
+          <img src={placeholder} alt="Libro 3" />
           <h3>Titolo Libro 3</h3>
           <p>Autore: Giovanni Verdi</p>
           <button class="btn">Acquista Ora</button>
